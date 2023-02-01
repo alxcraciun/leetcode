a43a96f957419af46122daf21cbdadc403144a7c
AlexCraciun <56975249+alxcraciun@users.noreply.github.com>
2023-02-01 12:02:45 +0200

    Symmetric Tree

--- 0101.py
+++ 0101.py
@@ -6,18 +6,57 @@ class TreeNode:
         self.right = right
 
 # Recursive Solution
-class Solution:
+class Solution1:
+    def helper(self, leftRoot: TreeNode, rightRoot: TreeNode) -> bool:
+        if leftRoot is None and rightRoot is None:
+            return True
+
+        if (leftRoot is None and rightRoot is not None) or (leftRoot is not None and rightRoot is None):
+            return False
+
+        if leftRoot.val != rightRoot.val:
+            return False
+
+        return self.helper(leftRoot.left, rightRoot.right) and self.helper(leftRoot.right, rightRoot.left)
+
     def isSymmetric(self, root: TreeNode) -> bool:
-        stack = []
+        return self.helper(root.left, root.right)
         
+''' Nested Function Recursive Solution
+class Solution1:
+    def isSymmetric(self, root: Optional[TreeNode]) -> bool:
+        def helper(left, right):
+            if not left and not right:
+                return True
+            if not left or not right:
+                return False
+            return (left.val == right.val) and helper(left.left, right.right) and helper(left.right, right.left)
+        return helper(root.left, root.right)
+'''
 
 # Iterative Solution
 class Solution2:
     def isSymmetric(self, root: TreeNode) -> bool:
-        pass
+        stack = [(root.left, root.right)]
+        while stack:
+            left, right = stack.pop()
+            if (left and right) and (left.val == right.val):
+                stack.append((left.right, right.left))
+                stack.append((left.left, right.right))
+            elif not left and not right:
+                continue
+            else:
+                return False
+        return True
 
-solution_recursive = Solution()
+# Example
+head = TreeNode(1)
+head.left = TreeNode(2)
+head.right = TreeNode(2)
+head.left.left = TreeNode(3)
+head.right.right = TreeNode(3)
+head.left.right = TreeNode(4)
+head.right.left = TreeNode(4)
 
-
-solution_iteration = Solution()
-
+answer = Solution2()
+print(answer.isSymmetric(head))
